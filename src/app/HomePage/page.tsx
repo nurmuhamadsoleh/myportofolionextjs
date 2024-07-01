afbd7b56c6cc6cda1abfb3738a9fe0c014c228a8
nursoleh33 <nursoleh33@gmail.com>
2024-07-01 23:26:08 +0700

    update root myportofolio

--- src/app/HomePage/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import HomePageContainer from '@/module/HomePage/Container/HomePageContainer'
-import React from 'react'
-
-export default function pages() {
-  return <HomePageContainer />
-}
